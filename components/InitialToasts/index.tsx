c5fba7736f1db002f1a4429be446001450423871
Paulo-Mikhael <paulomiguel11111971@gmail.com>
2024-10-24 14:22:26 -0400

    escondendo botão

--- components/InitialToasts/index.tsx
+++ components/InitialToasts/index.tsx
@@ -7,24 +7,21 @@ export function InitialToasts() {
   const { toast } = useToast();
   const buttonRef = useRef<HTMLButtonElement | null>(null);
 
-  function showToast() {
-    toast({
-      title: "Personalize seu Dashboard!",
-      description:
-        "Calcule seu IMC e selecione o nível de atividade física em que você se enquadra para obter suas informações nutricionais.",
-    });
-  }
-
   useEffect(() => {
     buttonRef.current?.click();
   }, []);
 
   return (
     <button
+      hidden
       ref={buttonRef}
       type="button"
       onClick={() => {
-        showToast();
+        toast({
+          title: "Personalize seu Dashboard!",
+          description:
+            "Calcule seu IMC e selecione o nível de atividade física em que você se enquadra para obter suas informações nutricionais.",
+        });
       }}
     >
       Enviar
